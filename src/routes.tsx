19fedd2748d0ac0a2d037cf5d06614e0202bc042
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2024-12-05 16:45:07 -0400

    initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Perfil from './Pages/Perfil';
+
+const Rotas = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/perfil" element={<Perfil />} />
+    <Route path="/restaurant/:id" element={<Perfil />} />
+  </Routes>
+);
+
+export default Rotas;
